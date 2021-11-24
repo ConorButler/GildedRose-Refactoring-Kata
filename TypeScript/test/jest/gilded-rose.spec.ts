61068f06907b1aa71796382029db4d17eac750b9
ConorButler <cwdbutler@gmail.com>
2021-11-24 21:39:09 +0000

    First test

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,39 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe("Gilded Rose", () => {
+  it("the quality of an item is never negative", () => {
+    let item = new Item("foo", 1, 1);
+    let gildedRose = new GildedRose([item]);
+
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+
+    expect(item.quality).toBe(0);
   });
+
+  test.todo(
+    "the quality of an item is never increased above 50 unless it is Sulfuras"
+  );
+
+  test.todo("once the sell by date has passed, quality degrades twice as fast");
+
+  test.todo("Aged Brie increases in quality the older it gets");
+
+  test.todo("the quality of an item is never more than 50");
+
+  test.todo("Sulfuras never has to be sold or decreases in quality");
+
+  test.todo("Sulfuras quality is 80 and never changes");
+
+  test.todo(
+    "Backstage passes' quality increase by 2 when there are 10 days left"
+  );
+
+  test.todo(
+    "Backstage passes' quality increase by 3 when there are 5 days or less"
+  );
+
+  test.todo("Backstage passes' quality drops to 0 after SellIn reaches 0");
+
+  test.todo("Conjured items degrade in quality twice as fast as normal items");
 });
